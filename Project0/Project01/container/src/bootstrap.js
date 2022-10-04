a5348b1bf4e38fe3c2c4c93208ff5174f0244ed5
Shaksham Sinha <shaksham@Shaksham-Sinhas-MacBook-Pro.local>
2022-10-04 21:13:57 +0530

    Renaming imports✅

--- Project0/Project01/container/src/bootstrap.js
+++ Project0/Project01/container/src/bootstrap.js
@@ -1,7 +1,7 @@
-import { mount } from "products/ProductsIndex";
-import { mount as mountCart } from "cart/CartShow";
+import { mount as productsMount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("Container!");
 
-mount(document.querySelector("#my-products"));
-mountCart(document.querySelector("#my-cart"));
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
