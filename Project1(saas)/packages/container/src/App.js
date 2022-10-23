8f453fc285e27fff82b2314617e77cdb02e7f00a
shaksham08 <shakshamsinha@outlook.com>
2022-10-23 16:19:15 +0530

    Added complete dashboard ⚡️

--- Project1(saas)/packages/container/src/App.js
+++ Project1(saas)/packages/container/src/App.js
@@ -1,23 +1,33 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from "history";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -32,6 +42,11 @@ export default () => {
                     setIsSignedIn(true);
                   }}
                 />
+                n
+              </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
               </Route>
               <Route path="/">
                 <MarketingLazy />
@@ -40,6 +55,6 @@ export default () => {
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
--- Project1(saas)/packages/dashboard/config/webpack.dev.js
+++ Project1(saas)/packages/dashboard/config/webpack.dev.js
@@ -7,7 +7,7 @@ const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   output: {
-    publicPath: "http://localhost:8083 /",
+    publicPath: "http://localhost:8083/",
   },
   devServer: {
     port: 8083,
